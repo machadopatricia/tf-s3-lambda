3bd43548b340c4b09ddf2a1d74cc8041629f4045
machadopatricia <patsouzam@gmail.com>
2023-05-02 02:05:56 -0300

    testing-js-changes-3

--- lambda_hello_world_function.js
+++ lambda_hello_world_function.js
@@ -2,7 +2,7 @@ exports.handler = async (event) => {
   const response = {
     statusCode: 200,
     headers: { 'Content-Type': 'text/html' },
-    body: '<h1>Hello, World!</h1>',
+    body: '<h1>Hello, World! test: triggering workflow when js files changes</h1>',
   };
   return response;
 };

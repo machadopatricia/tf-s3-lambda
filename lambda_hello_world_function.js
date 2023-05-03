5b0b71505bf4ef5fa4dc48a0629fe3b3668673a2
machadopatricia <patsouzam@gmail.com>
2023-05-03 02:58:23 -0300

    testing-zip-js-workflow-10

--- lambda_hello_world_function.js
+++ lambda_hello_world_function.js
@@ -2,7 +2,7 @@ exports.handler = async (event) => {
   const response = {
     statusCode: 200,
     headers: { 'Content-Type': 'text/html' },
-    body: '<h1>Hello, World! test 11: zipping file when it changes</h1>',
+    body: '<h1>Hello, World! test 12: zipping file when it changes</h1>',
   };
   return response;
 };
